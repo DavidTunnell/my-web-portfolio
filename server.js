35eb8f3848885309a3d34468fc35b941420b9ea5
David Tunnell <davidtunnell9@gmail.com>
2021-10-01 14:50:14 -0500

    express server now rendering site and email API call works

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 // Sets up the Express App
 const app = express();
@@ -11,8 +13,36 @@ app.use(express.urlencoded({ extended: false }));
 //set public folder
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/email", function (req, res) {
-    res.send("hello world");
+// create reusable transporter object using the default SMTP transport
+const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+        user: process.env.GMAIL_ADDRESS,
+        pass: process.env.GMAIL_PASSWORD, //if you have 2 factor authentication on: https://stackoverflow.com/a/49306726/1524210
+    },
+    secure: true,
+});
+
+app.post("/text-email", (req, res) => {
+    const { from, subject, text } = req.body;
+    const mailData = {
+        from: from,
+        to: process.env.GMAIL_ADDRESS,
+        subject: subject,
+        text: text,
+        html: "<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>",
+    };
+
+    transporter.sendMail(mailData, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        res.status(200).send({
+            message: "Mail send",
+            message_id: info.messageId,
+        });
+    });
 });
 
 app.listen(PORT, () => {
