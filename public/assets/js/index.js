170d25faf467940d29e577b9586e91239a9362d4
David Tunnell <davidtunnell9@gmail.com>
2021-10-01 16:24:26 -0500

    form done

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,6 @@
+const formFeedbackSpan = document.querySelector(".form-feedback");
+const contactForm = document.querySelector(".contact-form");
+
 const submitHandler = async (event) => {
     event.preventDefault();
     const name = document.querySelector(".user-input-name").value.trim();
@@ -7,6 +10,12 @@ const submitHandler = async (event) => {
         .value.trim();
     const message = document.querySelector(".user-input-message").value.trim();
 
+    if (!validateEmail(email)) {
+        formFeedbackSpan.innerHTML = "Please enter a valid email address.";
+        formFeedbackSpan.style.color = "#FFCFCF";
+        return;
+    }
+
     if (name && email && phoneNumber && message) {
         const response = await fetch("/text-email", {
             method: "POST",
@@ -14,14 +23,24 @@ const submitHandler = async (event) => {
             headers: { "Content-Type": "application/json" },
         });
         if (response.ok) {
-            console.log("email ok");
+            formFeedbackSpan.innerHTML = "Message successfully received!";
+            formFeedbackSpan.style.color = "#C6FFBE";
+            contactForm.reset();
         } else {
-            console.log("email fail");
+            formFeedbackSpan.innerHTML = "Error sending email.";
+            formFeedbackSpan.style.color = "#FFCFCF";
         }
     } else {
-        alert("Please fill out all fields.");
+        formFeedbackSpan.innerHTML = "Please fill out all fields.";
+        formFeedbackSpan.style.color = "#FFCFCF";
     }
 };
+
+function validateEmail(email) {
+    const re =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 
 const submitButton = document
     .querySelector(".form-button")
