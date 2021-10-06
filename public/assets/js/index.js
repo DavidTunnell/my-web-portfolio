b7187b084609201ce9db9e05bfbeb8e4e4e864e4
David Tunnell <davidtunnell9@gmail.com>
2021-10-06 14:30:51 -0500

    added ability to go to either repo or demo links for projects

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,8 @@
 const formFeedbackSpan = document.querySelector(".form-feedback");
 const contactForm = document.querySelector(".contact-form");
+const projects = [...document.querySelector(".project-items").children];
 
+//on submit contact form handler (sends email via gmail servers and provides validation)
 const submitHandler = async (event) => {
     event.preventDefault();
     const name = document.querySelector(".user-input-name").value.trim();
@@ -35,12 +37,35 @@ const submitHandler = async (event) => {
     }
 };
 
+//validates email string
 function validateEmail(email) {
     const re =
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
+//hides project title text on mouse over and shows again when mouse removed
+projects.forEach((element) => {
+    const currentProjectTitle = element.querySelector(".project-text");
+    if (currentProjectTitle) {
+        element.addEventListener(
+            "mouseover",
+            function () {
+                currentProjectTitle.setAttribute("style", "display:none;");
+            },
+            false
+        );
+        element.addEventListener(
+            "mouseout",
+            function () {
+                currentProjectTitle.setAttribute("style", "");
+            },
+            false
+        );
+    }
+});
+
+//add event handler to contact submit button
 const submitButton = document
     .querySelector(".form-button")
     .addEventListener("click", submitHandler);
